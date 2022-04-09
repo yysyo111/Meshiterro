3c70dae96a5efd33595f03a3cc8cfe2e167eb6a9
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-09 14:38:19 +0000

    [Finish]deviseを使用した実装(3)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'homes/top'
   root to: "homes#top"
-  # root to:はサイトのルートページを決める記述
+  get 'homes/about', as: 'about'
   
+  # root to:はサイトのルートページを決める記述
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
