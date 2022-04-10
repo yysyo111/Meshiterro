41858ac448c5fba67ffda3141a2da6bd1c59bb9d
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-04-10 13:15:25 +0000

    [Finish]モデルを準備しよう(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :post_images, dependent: :destroy
 end
 
 # 「作成した User モデルに devise で使用する機能が記述されている」
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   # devise_for :users は、devise を使用する際に URL として users を含むことを示しています
   # get 'homes/top'
   root to: "homes#top"
+  
+  # get 'post_images/new'
+  # get 'post_images/index'
+  # get 'post_images/show'
+  resources :post_images, only: [:new, :index, :show]
+  
   get 'homes/about', as: 'about'
+  # get "/homes/about" => "homes#about", as: "about"
   
   # root to:はサイトのルートページを決める記述
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
